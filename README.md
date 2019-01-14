f57ecbbaa23d015349b81fc8eee4e72265063909
barrycunningham <cunnbarry@gmail.com>
2019-01-14 17:31:15 +0000

    Data connected, first chart working

---
+++ README.md
@@ -0,0 +1,29 @@
+
+     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.
+    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'
+    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, 
+    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.
+     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
+    ----------------------------------------------------------------- 
+
+
+Hi there! Welcome to Cloud9 IDE!
+
+To get you started, create some files, play with the terminal,
+or visit http://docs.c9.io for our documentation.
+If you want, you can also go watch some training videos at
+http://www.youtube.com/user/c9ide.
+
+Happy coding!
+The Cloud9 IDE team
+
+
+
+
+$ http GET "https://www.strava.com/api/v3/activities/{id}?include_all_efforts=" "Authorization: Bearer [[token]]"
+
+Client ID	31571
+
+Cient Access Token     5e29c53673ecb5de42afa2c32b88a799c739edd3
+
+$ http GET "https://www.strava.com/api/v3/activities/{31571}?include_all_efforts=" "Authorization: Bearer [[5e29c53673ecb5de42afa2c32b88a799c739edd3]]"
---
+++ assets/js/barry.strava.js
@@ -0,0 +1,75 @@
+queue()
+    .defer(d3.csv, "data/barry.strava.csv")
+    .await(makeGraphs);
+    
+function makeGraphs(error, barryActivities) {
+    var ndx = crossfilter(barryActivities);
+    
+    show_type_balance(ndx);
+    
+    dc.renderAll();
+}
+
+function show_type_balance(ndx) {
+    var dim = ndx.dimension(dc.pluck('type'));
+    var group = dim.group();
+    
+    dc.barChart("#type")
+        .width(400)
+        .height(200)
+        .margins({top:10, right: 10, bottom: 30, left: 30})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Type")
+        .yAxisLabel("Amount")
+        .yAxis().ticks(10);
+}
+
+
+// // const Http = new XMLHttpRequest();
+// // const url='https://www.strava.com/api/v3/activities/{1655013} Authorization: Bearer 5e29c53673ecb5de42afa2c32b88a799c739edd3';
+// // Http.open("GET", url);
+// // Http.send();
+// // Http.onreadystatechange=(e)=>{
+// // console.log(Http.responseText)
+// // }
+
+
+// var chart = dc.scatterPlot('#distance');
+
+// d3.csv("data/barry.strava.csv", function(errors, distance) {
+//     var mycrossfilter = crossfilter(distance);
+
+//     // people.forEach(function(x) {
+//     //     if (x.male == 1) {
+//     //         x.gender = "Male";
+//     //     }
+//     //     else {
+//     //         x.gender = "Female";
+//     //     }
+//     // });
+
+//     var hwDimension = mycrossfilter.dimension(function(data) {
+//         return [Math.floor(data.date), Math.floor(data.distance)];
+//     });
+//     var hwGroup = hwDimension.group().reduceCount();
+
+//     chart
+//         .width(800)
+//         .height(600)
+//         .x(d3.scale.linear().domain([0, 200000]))
+//         .y(d3.scale.linear().domain([0, 200000]))
+//         .brushOn(false)
+//         .xAxisLabel("Date")
+//         .yAxisLabel("Distance")
+//         .symbolSize(8)
+//         .clipPadding(10)
+//         .dimension(hwDimension)
+//         .group(hwGroup);
+
+//     chart.render();
+// });

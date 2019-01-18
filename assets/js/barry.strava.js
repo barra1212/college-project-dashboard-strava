0a473878630a1c19943b7c4feb3971c961e21aa1
barrycunningham <cunnbarry@gmail.com>
2019-01-18 13:48:13 +0000

    Footer Scroll to top added

--- assets/js/barry.strava.js
+++ assets/js/barry.strava.js
@@ -185,3 +185,25 @@ function show_average_speed(ndx) {
         .xAxisLabel("Month")
         .yAxis().ticks(10);
 }
+
+
+// 
+
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
